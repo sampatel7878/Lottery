7739ea1152953a6ebe45fcc7b79970fb348094f8
sampatel7878 <67911824+sampatel7878@users.noreply.github.com>
2022-01-21 13:00:13 +0530

    Create Lottery.sol

---
+++ Lottery.sol
@@ -0,0 +1,37 @@
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract Lottery {
+    address payable[] public players;
+    address public manager;
+    uint lotteryAmount;
+    uint public noOfPlayer;
+    uint deadline;
+    uint public collectedAmount;
+    constructor (uint _lotterAmount, uint _deadline) {
+        manager = msg.sender;
+        lotteryAmount = _lotterAmount;
+        deadline = block.timestamp + _deadline;
+    }
+
+    function sendEth()public payable{
+        require(block.timestamp <= deadline,"Deadline has passed");
+        require(msg.value == lotteryAmount,"Minimum Amount not met");
+        players.push(payable(msg.sender));
+        collectedAmount += msg.value;
+        noOfPlayer++;
+    }
+    function currentPoolBalance() view public returns(uint){
+        return address(this).balance;
+    }
+
+    function pickWinner() public{
+        require(msg.sender == manager,"Only manager can call this function");
+        require(players.length >= 3,"3 Players required");
+        uint random = uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+
+        address payable winner;
+        uint index = random % players.length;
+        winner= players[index];
+        winner.transfer(address(this).balance);
+    }
+}
